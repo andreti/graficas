852ac820c1087b0094e90f827a264316088658bc
Andres <casz21@hotmail.com>
2015-02-28 19:23:44 -0500

    desde casa 28 febrer

--- javas.js
+++ javas.js
@@ -6,17 +6,16 @@ var y=parseFloat(alto/2);
 
 
 function dibujarPlano(){
-	c = document.getElementById("canvas");
-	c.width =ancho;
-	c.height = alto;
-	c.style.border="1px solid skyblue";
 
-	ctx = c.getContext("2d");
+	canvas.width =ancho;
+	canvas.height = alto;
+	canvas.style.border="1px solid skyblue";
+
 	ctx.fillStyle = "#FF0000";
 
 	ctx.moveTo(0,parseFloat(y));
 	ctx.lineTo(parseFloat(x*2),parseFloat(y));
-	ctx.stroke();
+	//ctx.stroke();
 
 	ctx.moveTo(parseFloat(x),0);
 	ctx.lineTo(parseFloat(x),parseFloat(y*2));
@@ -27,8 +26,95 @@ function concatenar(e){
 	cadena+=e.target.id;
 	resultado.value = cadena;
 }
+function funcionCuadratica(){
+
+}
+function funcionLineal(f){
+	reporte = new Array;
+	j = 0;
+
+	funcion = f;
+	funcion = "2*x+50";
+
+	x1i = null,	y1i = null,	x2f = null,	y2f = null;
+
+	for(i = 0; i<y ; i++){
+		p = "2*"+i+"+4";
+		//console.log(p);
+		yResultado = eval(p);
+		negativo = -yResultado;
+		////console.log("+Y: "+yResultado);
+		if(j<=10) {
+			reporte[j] = "x = "+i+" ; y = "+yResultado;
+			j++;
+		}
+		yResultado = eval((-1)*(yResultado)) ;	
+		yString = y+yResultado;
+		console.log(yString);	
+		//yString = yResultado < 0 ? y+yResultado : y+"+"+yResultado;
+
+		//console.log(yString);
+		//yResultado = eval(yString);
+		//////console.log("+Y: "+yResultado);
+		x1i = parseFloat(x+i);
+		y1i =  eval(yString);
+
+		//console.log("x1i:"+x1i+", y1i: "+y1i+", Resultado:"+yResultado+" i:"+i);
+		ii = -i;
+		p = "2*"+ii+"+50";
+		yResultado = eval(p);
+		//console.log("-Y: "+yResultado);
+		//yResultado = negativo;
+		if(j<=10 && i!=0) {
+			reporte[j] = "x = "+"-"+i+" ; y = "+yResultado;
+			j++;
+		}
+		//yResultado = yResultado < 0 ? eval((-1)*(yResultado)):eval((-1)*(yResultado)) ; 
+		yResultado = eval((-1)*(yResultado)) ;	
+		yString = y+yResultado;
+		console.log(yString);
+		//yString = yResultado < 0 ? y+yResultado:y+"+"+yResultado;
+		//console.log(yString);
+		//yResultado = eval(yString);
+		////console.log("-Y: "+yResultado);
+		x2f = parseFloat(x-i);
+		y2f = eval(yString);
+
+		//console.log("x2f:"+x2f+", y2f: "+y2f+", Resultado: "+yResultado);
+
+	}
+	ctx.moveTo(x1i,y1i);
+	ctx.lineTo(x2f,y2f);
+	ctx.stroke();
+	imprimirReporte(funcion,reporte);
+
+}
+function imprimirReporte(funcion ,arrayReporte){
+	document.getElementById("reporte").innerHTML+="Ecuación: "+funcion+"<br>";
+	for(i =0 ; i< arrayReporte.length ; i++)
+		document.getElementById("reporte").innerHTML+=arrayReporte[i]+"<br>";
+}
+function dibujarPunto(){
+
+}
+function limpiar(){
+	document.getElementById("reporte").innerHTML="";
+	/*resultado.value="";
+	cadena ="";*/
+}
 function igual(){
-	c = document.getElementById("canvas");
+	limpiar();
+	ctx.lineWidth = 0.5;
+	ctx.fillStyle = "red";
+	funcion = resultado.value;
+	if(funcion!=""){
+		if(funcion.indexOf("^")>-1)
+			funcionCuadratica(funcion);
+		else if(funcion.indexOf("x")>-1)
+			funcionLineal(funcion);
+	}
+
+	/*c = document.getElementById("canvas");
 	ctx = c.getContext("2d");
 	ctx.fillStyle = "red";
 	x1 =0;
@@ -41,7 +127,8 @@ function igual(){
 	funcion = "2x+2+2";
 	funcion = funcion.split("x");
 	for(i = 0 ;i<funcion.length;i++){
-		funcion2 +=funcion[i]
+		funcion2 +=funcion[i];
+		funcion2 += 
 	}
 	for(i = 0; i<y;i++){
 		console.log("-------------------------------------------");
@@ -83,7 +170,7 @@ function igual(){
 		y1 = yResultado;
 		ctx.lineTo(x1,y1);
 		ctx.stroke();
-	}
+	}*/
 	
 }
 function grafica(){
@@ -111,6 +198,10 @@ function cargarDoc(){
 	document.getElementById("x2").addEventListener("click",concatenar);
 	document.getElementById("igual").addEventListener("click",igual);
 	resultado = document.getElementById("in");
+
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
+
 	dibujarPlano();
 	//grafica();
 
