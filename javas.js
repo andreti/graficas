1093be19ff359007f40a7208be2a16ad89937561
Andres <casz21@hotmail.com>
2015-03-06 13:47:19 -0500

    6/3

--- javas.js
+++ javas.js
@@ -1,9 +1,11 @@
 var cadena = "",
 	resultado=null,
 	ancho=parseFloat(400),
-	alto=parseFloat(300),
+	alto=parseFloat(ancho),
 	x=parseFloat(ancho/2),
 	y=parseFloat(alto/2),
+	valorT=parseFloat(ancho/40);
+	pixel = 0.05;
 	ok = false;
 
 function dibujarPlano(){
@@ -12,6 +14,8 @@ function dibujarPlano(){
 	canvas.height = alto;
 	canvas.style.border="1px solid skyblue";
 
+	ctx.beginPath();
+
 	ctx.fillText("X",0,parseFloat(y));
 	ctx.moveTo(0,parseFloat(y));
 	ctx.lineTo(parseFloat(x*2),parseFloat(y));
@@ -19,6 +23,8 @@ function dibujarPlano(){
 	ctx.fillText("Y",parseFloat(x),10);
 	ctx.moveTo(parseFloat(x),0);
 	ctx.lineTo(parseFloat(x),parseFloat(y*2));
+
+
 
 	ctx.lineWidth = 1;
 	ctx.strokeStyle = '#C1C1C1';
@@ -31,7 +37,7 @@ function concatenar(e){
 }
 
 function concatenarCuadratica(funcion){
-	return funcion.replace(/^2/g,"*1*x+1*1+x*1");
+	return funcion.replace(/c/g,"*1*(x*x)*1");
 }
 function concatenarLineal(funcion){
 	return funcion.replace(/x/g,"*1*x*1");
@@ -46,19 +52,108 @@ function concatenarCos(){
 
 }
 function dibujar(funcion){
+	funcionOrigina = funcion;
+	por = funcion.indexOf("x")==0?"":"*";
 	if(funcion.indexOf("x")>-1)
-		funcion = concatenarLineal(funcion);
-	if(funcion.indexOf("^2")>-1)
-		funcion = concatenarCuadratica(funcion);
-	for(i = -100 ; i < 100 ; i++){
-		f = funcion.replace(/x/g,i);
-		res = eval(f);
-		if(i == -100)
-			ctx.moveTo(x-i,y+res);
-		ctx.lineTo(x-i,y+res);
-		ctx.stroke();
+		funcion = funcion.replace(/x/g,por+"1*x");
+	if(funcion.indexOf("c")>-1)
+		funcion = funcion.replace(/c/g,por+"1*(x*x)");
+	while(funcion.indexOf("(*")>-1)
+		funcion = funcion.replace('(*',"(");
+	if(funcion.indexOf("sen(")>-1){
+		console.log(funcion.indexOf("sen("));
+	}
+/*
+ sen(*1*x)+2*1*x
+ *0.5440211108893698(*1*x)+2*1*x
+ *0.5440211108893698(*1*-10)+2*1*-10
+*/
+	ctx.beginPath();
+	j = 0;
+	console.log(funcion);
+	reporte = new Array();
+	for(i = -valorT ; i < valorT ; i+=pixel){
+		funcion = funcion.replace(/sen/g, por+Math.sin(i) );
+		console.log(funcion);
+		funcion = funcion.replace(/x/g,i);
+		console.log(funcion);
+		res = eval(funcion);
+		console.log(funcion);
+		res = eval(y+(res*-1)) ;	
+		if(i == -valorT)
+			ctx.moveTo(x+i,res);
+		ctx.lineTo(x+i,res);
+
+		if(j<=10) {
+			reporte[j] = "x = &nbsp;"+parseFloat(x+i)+"&nbsp;&nbsp;&nbsp;,&nbsp;&nbsp; y = &nbsp;"+res;
+			j++;
+		}
+	}
+
+	graficar();
+	imprimirReporte(funcionOrigina, reporte);
+	
+}
+function graficar(){
+	ctx.lineWidth = 2;
+	ctx.strokeStyle = '#000';
+	ctx.stroke();
+}
+function imprimirReporte(funcion ,arrayReporte){
+	document.getElementById("reporte").innerHTML+="Ecuación: "+funcion+"<br>";
+	for(i =0 ; i< arrayReporte.length ; i++)
+		document.getElementById("reporte").innerHTML+=arrayReporte[i]+"<br>";
+}
+function limpiar(){
+	document.getElementById("reporte").innerHTML="";
+	resultado.value="";
+	cadena ="";
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    dibujarPlano();
+	ok = false;
+}
+function igual(){
+	if(ok)
+		limpiar();
+	ctx.lineWidth = 0.5;
+	ctx.fillStyle = "red";
+	funcion = resultado.value;
+	if(funcion!=""){
+		if(funcion.indexOf("^")>-1)
+			funcionCuadratica(funcion);
+		else if(funcion.indexOf("x")>-1)
+			funcionLineal(funcion);
 	}
 }
+
+function cargarDoc(){
+	for(i = 0 ; i<10 ; i++)
+		document.getElementById(i).addEventListener("click",concatenar);
+
+	document.getElementById("+").addEventListener("click",concatenar);
+	document.getElementById("-").addEventListener("click",concatenar);
+	document.getElementById("*").addEventListener("click",concatenar);
+	document.getElementById("/").addEventListener("click",concatenar);
+
+	document.getElementById("x").addEventListener("click",concatenar);
+	document.getElementById("^").addEventListener("click",concatenar);
+	document.getElementById("igual").addEventListener("click",igual);
+
+	document.getElementById("CE").addEventListener("click",limpiar);
+	anchoSection = ancho+5;
+	document.getElementById("section").style.width=anchoSection+"px";
+	resultado = document.getElementById("in");
+
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
+	dibujarPlano();
+	//dibujar("2c+3c+100");
+	//dibujar("2x+10");
+	dibujar("sen(x)+2x+sen(x)");
+	//graficar();
+
+}
+window.addEventListener("load",cargarDoc);
 
 
 
@@ -201,56 +296,3 @@ function funcionCuadratica(){
 	ok = true;
 }*/
 
-function imprimirReporte(funcion ,arrayReporte){
-	document.getElementById("reporte").innerHTML+="Ecuación: "+funcion+"<br>";
-	for(i =0 ; i< arrayReporte.length ; i++)
-		document.getElementById("reporte").innerHTML+=arrayReporte[i]+"<br>";
-}
-function limpiar(){
-	document.getElementById("reporte").innerHTML="";
-	resultado.value="";
-	cadena ="";
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    dibujarPlano();
-	ok = false;
-}
-function igual(){
-	if(ok)
-		limpiar();
-	ctx.lineWidth = 0.5;
-	ctx.fillStyle = "red";
-	funcion = resultado.value;
-	if(funcion!=""){
-		if(funcion.indexOf("^")>-1)
-			funcionCuadratica(funcion);
-		else if(funcion.indexOf("x")>-1)
-			funcionLineal(funcion);
-	}
-}
-
-function cargarDoc(){
-	for(i = 0 ; i<10 ; i++)
-		document.getElementById(i).addEventListener("click",concatenar);
-
-	document.getElementById("+").addEventListener("click",concatenar);
-	document.getElementById("-").addEventListener("click",concatenar);
-	document.getElementById("*").addEventListener("click",concatenar);
-	document.getElementById("/").addEventListener("click",concatenar);
-
-	document.getElementById("x").addEventListener("click",concatenar);
-	document.getElementById("^").addEventListener("click",concatenar);
-	document.getElementById("igual").addEventListener("click",igual);
-
-	document.getElementById("CE").addEventListener("click",limpiar);
-	anchoSection = ancho+5;
-	document.getElementById("section").style.width=anchoSection+"px";
-	resultado = document.getElementById("in");
-
-	canvas = document.getElementById("canvas");
-	ctx = canvas.getContext("2d");
-	dibujarPlano();
-	dibujar("2^2+3");
-	//graficar();
-
-}
-window.addEventListener("load",cargarDoc);
